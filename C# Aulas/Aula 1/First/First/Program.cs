d0e28f3f4b3eb7daef2c3a6e47fc29168b90f545
Davi Profeta <davihpg2008@gmail.com>
2026-07-03 15:09:06 -0300

    Add files via upload

---
+++ C# Aulas/Aula 1/First/First/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao game");
+            Console.WriteLine();
+            Console.WriteLine("Guerreiro");
+            Console.WriteLine("Mago");
+            Console.WriteLine("Arqueiro");
+            Console.WriteLine();
+            Console.Write("Escreva sua classe: ");
+            string clase = Console.ReadLine();
+
+            if (clase == "Guerreiro" || clase == "Mago" || clase == "Arqueiro")
+            {
+                Console.WriteLine("Bem Vindo");
+            }
+            else
+            {
+                Console.WriteLine("Erro, tente novamente");
+            }
+            
+        }
+    }
+}

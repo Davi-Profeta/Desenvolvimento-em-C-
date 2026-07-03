cbf25968d357c0803c703e4e8b267a0d5b5d326c
Davi Profeta <davihpg2008@gmail.com>
2026-07-03 15:17:01 -0300

    Add files via upload

---
+++ C# Aulas/Aula 1/Atv14/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Atv14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você precisa de 150 critais para passar de missão");
+            Console.Write("Quantos cristais você: ");
+            int crytals = Convert.ToInt32(Console.ReadLine());
+
+            if(crytals < 150)
+            {
+                Console.WriteLine("Você precisa de mais "+(150 - crytals));
+            }
+            else if(crytals == 150)
+            {
+                Console.WriteLine("Você passou");
+            }
+            else if( crytals > 150)
+            {
+                Console.WriteLine("Você passou de fase");
+                Console.WriteLine("Sobraram "+ (crytals - 150));
+            }
+
+        }
+    }
+}
---
+++ C# Aulas/Aula 1/Atv15/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Atv15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("============ Final secreto ==========");
+            Console.WriteLine();
+            Console.Write("Você derrotou o boos final (sim ou não): ");
+            string defeated = Console.ReadLine();
+
+            if(defeated == "SIM" ||
+                defeated == "sim" ||
+                defeated == "Sim")
+            {
+
+                Console.Write("Você encontrou a reliquia (sim ou não): ");
+                string relic = Console.ReadLine();
+
+                if (relic == "SIM" ||
+                relic == "sim" ||
+                relic == "Sim")
+                {
+                    Console.WriteLine("Final verdadeiro");
+                }
+                else if (relic == "NÃO" ||
+                relic == "não" ||
+                relic == "Não")
+                {
+                    Console.WriteLine("Final normal");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                }
+            }
+            else if(defeated == "NÃO" ||
+                defeated == "não" ||
+                defeated == "Não")
+            {
+                Console.WriteLine("Continue sua aventura");
+            }
+            else
+            {
+                Console.WriteLine("Errom");
+            }
+
+
+        }
+    }
+}

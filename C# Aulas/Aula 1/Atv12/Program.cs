e825bcdd2372914c0fcba993539b60adb7cca1c3
Davi Profeta <davihpg2008@gmail.com>
2026-07-03 15:16:06 -0300

    Add files via upload

---
+++ C# Aulas/Aula 1/Atv12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace Atv12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int strength = 0;
+            int speed = 0;
+            int defense = 0;
+            
+            Console.WriteLine("========== EVOLUÇÃO =========");
+            Console.WriteLine("O que você vai evoluir?");
+            Console.WriteLine();
+            Console.WriteLine("Força - " +strength);
+            Console.WriteLine("Defesa - " +defense);
+            Console.WriteLine("Velocidade -" +speed);
+            Console.WriteLine();
+            Console.Write("Digite qual quer evoluir: ");
+            string attribute = Console.ReadLine();
+
+            if(attribute == "Força" ||
+                attribute == "força" ||
+                attribute == "FORÇA")
+            {
+                Console.WriteLine("Sua força aumentou em 10");
+                strength += 10;
+            }
+            else if (attribute == "Defesa" ||
+                attribute == "defesa" ||
+                attribute == "DEFESA")
+            {
+                Console.WriteLine("Sua Defesa aumentou em 10");
+                defense += 10;
+            }
+            else if (attribute == "Velocidade" ||
+                   attribute == "velocidade" ||
+                   attribute == "VELOCIDADE")
+            {
+                Console.WriteLine("Sua Velocidade aumentou em 10");
+                speed += 10;
+            }
+            else
+            {
+                Console.WriteLine("Não exite essa habilidade");
+            }
+
+
+        }
+    }
+}
---
+++ C# Aulas/Aula 1/Atv13/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Atv13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========== Portais ===========");
+            Console.WriteLine();
+            Console.WriteLine("1 - Gelo");
+            Console.WriteLine("2 - Fogo");
+            Console.WriteLine("3 - Floresta");
+            Console.WriteLine("4 - Deserto");
+            Console.WriteLine();
+            Console.Write("Qual porta você deseja: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num == 1)
+            {
+                Console.WriteLine("Está frio aí né");
+            }
+            else if(num == 2)
+            {
+                Console.WriteLine("Está quente aqui");
+            }
+            else if(num == 3)
+            {
+                Console.WriteLine("Shhi, tem animais perigosos aqui perto");
+            }
+            else if( num == 4)
+            {
+                Console.WriteLine("Nuuu, que calor");
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+            }
+
+        }
+    }
+}

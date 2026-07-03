7d8a91c305a2dfac098930e3267f54eac6f2d18f
Davi Profeta <davihpg2008@gmail.com>
2026-07-03 15:12:04 -0300

    Add files via upload

---
+++ C# Aulas/Aula 1/Atv5/Atv5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Atv5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int respawn = 10;
+
+            if(respawn > 0)
+            {
+                Console.WriteLine("Faltam " + respawn + " segundo para o respawn");
+            }
+            else if(respawn == 0)
+            {
+                Console.WriteLine("Jogador resnaceu");
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+            }
+        }
+    }
+}
---
+++ C# Aulas/Aula 1/Atv6/Atv6/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Atv6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua senha: ");
+            string password = Console.ReadLine();
+
+            if(password == "DRAGON")
+            {
+                Console.Write("Você possui a key (sim ou não): ");
+                string key = Console.ReadLine();
+
+                if(key == "sim" || key == "Sim" || key == "SIM")
+                {
+                    Console.WriteLine("Cofre aberto");
+                }
+                else if(key == "não" || key == "Não" || key == "NÃO")
+                {
+                    Console.WriteLine("Você precisa da key");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta");
+            }
+
+        }
+    }
+}

346d7b0b2e4729a023ca7c1f97ae28f66910cd7a
Davi Profeta <davihpg2008@gmail.com>
2026-07-03 15:10:35 -0300

    Add files via upload

---
+++ C# Aulas/Aula 1/Atv2/Atv2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Atv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quanto de HP o boss tem: ");
+            int HP = Convert.ToInt32(Console.ReadLine());
+
+            if(HP > 50)
+            {
+                Console.WriteLine("Boss saudável");
+            }
+            else if(HP <= 50 && HP > 0)
+            {
+                Console.WriteLine("Boos enfraquecido");
+            }
+            else if(HP == 0)
+            {
+                Console.WriteLine("Boos derrotado");
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido");
+            }
+        }
+    }
+}
---
+++ C# Aulas/Aula 1/Atv3/Atv3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Atv3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a loja");
+            Console.WriteLine();
+            Console.WriteLine("Itens disponíveis");
+            Console.WriteLine("Espada = 100 moedas");
+            Console.WriteLine("Arco = 150 moedas");
+            Console.WriteLine("Cajado = 200 moedas");
+
+            Console.WriteLine();
+
+            Console.Write("Quantas moedas você tem: ");
+            int moedas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Qual item você deseja: ");
+            string item = Console.ReadLine();
+
+            if (item == "Espada" || item == "Arco" || item == "Cajado")
+            {
+                if (item == "Espada" && moedas >= 100 ||
+                   item == "Arco" && moedas >= 150 ||
+                   item == "Cajado" && moedas >= 200)
+                {
+                    Console.WriteLine("Compra realizada");
+                }
+                else
+                {
+                    Console.WriteLine("Moedas insuficientes");
+                }
+                
+            }
+            else
+            {
+
+                Console.WriteLine("Esse item não existe");
+
+            }
+
+           
+
+        }
+    }
+}
